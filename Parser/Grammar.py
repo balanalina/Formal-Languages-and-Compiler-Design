ddeb108626e7cb72ef6b339efa49cb5d48550070
Balan Alina <alinabalan16.ab@gmail.com>
2021-01-12 13:54:18 +0200

    add lex

--- Parser/Grammar.py
+++ Parser/Grammar.py
@@ -1,1 +1,10 @@
-print("jjgj")
+class Grammar:
+    def __init__(self):
+        self.non_terminal = []
+        self.terminal = []
+        self.production = []
+        self.read_grammar()
+
+    def read_grammar(self):
+        pass
+
